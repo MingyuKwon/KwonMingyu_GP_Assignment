4d556432e023d8623424869b2a59860e3d3adb5c
Kwon Mingyu <kmgmg230601@gmail.com>
2024-10-27 15:26:42 +0900

    add navmesh system

--- Assets/Script/Enemy/EnemyMove.cs
+++ Assets/Script/Enemy/EnemyMove.cs
@@ -1,27 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    public int RandomLifeSpanStart = 3;
-    public int RandomLifeSpanEnd = 10;
     public float moveSpeed = 5.0f; 
 
+    private NavMeshAgent agent;
+    private Vector3 BasePosition;
     Vector3 targetposition;
     float targetDistance = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        int lifespan = Random.Range(RandomLifeSpanStart, RandomLifeSpanEnd);
-
         targetposition = transform.position;
+        BasePosition = GameObject.Find("Main Base").transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = true;
+
+        
+        agent.isStopped = false;
+        agent.SetDestination(BasePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 moveDirectrion = (targetposition - transform.position).normalized;
         transform.parent.Translate(moveDirectrion * moveSpeed * Time.deltaTime);
         targetDistance -= (moveDirectrion * moveSpeed * Time.deltaTime).magnitude;
@@ -33,6 +40,8 @@ public class EnemyMove : MonoBehaviour
 
         targetDistance = Vector3.Distance(targetposition , transform.position);
         targetposition = transform.position + new Vector3(randomX, 0 , randomZ);
+        */
+
 
 
     }
--- Assets/Script/System/RandomEnemySpawner.cs
+++ Assets/Script/System/RandomEnemySpawner.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RandomEnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+
+    public float sampleRadius = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +17,34 @@ public class RandomEnemySpawner : MonoBehaviour
         {
             if(enemyPrefab)
             {
-                GameObject clone = Instantiate(enemyPrefab);
-                clone.transform.position = transform.position;
-                clone.transform.rotation = transform.rotation;
+                SpawnEnemy(transform.position);
             }
         }
 
         Destroy(gameObject);
     }
+
+
+    void SpawnEnemy(Vector3 spawnPosition)
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(spawnPosition, out hit, sampleRadius, NavMesh.AllAreas))
+        {
+            GameObject enemy = Instantiate(enemyPrefab, hit.position, Quaternion.identity);
+            NavMeshAgent agent = enemy.GetComponent<NavMeshAgent>();
+
+            if (agent != null)
+            {
+                Vector3 targetPosition = GameObject.Find("Main Base").transform.position;
+                agent.SetDestination(targetPosition);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("유효한 NavMesh 위치를 찾지 못했습니다.");
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
