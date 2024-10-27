6079306fa159e8ae1b6eb4732dabebc6ffa8d562
Kwon Mingyu <kmgmg230601@gmail.com>
2024-10-27 14:51:29 +0900

    add move fast shift and stop crtl

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -18,10 +18,14 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         if(rigidbody == null) return;
+        if (Input.GetKey(KeyCode.LeftControl)) { rigidbody.velocity = Vector3.zero; return;}
 
-        if (Input.GetKey(KeyCode.W)) { rigidbody.AddRelativeForce(0, 0, speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.S)) { rigidbody.AddRelativeForce(0, 0, -speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.D)) { rigidbody.AddRelativeForce(speed * Time.deltaTime, 0, 0); }
-        if (Input.GetKey(KeyCode.A)) { rigidbody.AddRelativeForce(-speed * Time.deltaTime, 0, 0); }
+        float moveSpeed = speed;
+        if (Input.GetKey(KeyCode.LeftShift)) { moveSpeed = moveSpeed * 1.5f;}
+
+        if (Input.GetKey(KeyCode.W)) { rigidbody.AddRelativeForce(0, 0, moveSpeed * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.S)) { rigidbody.AddRelativeForce(0, 0, -moveSpeed * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.D)) { rigidbody.AddRelativeForce(moveSpeed * Time.deltaTime, 0, 0); }
+        if (Input.GetKey(KeyCode.A)) { rigidbody.AddRelativeForce(-moveSpeed * Time.deltaTime, 0, 0); }
     }
 }
